be7dbea282ee64eea51f6e0b1031ec0cde4350d8
RaymondzyLei <raymond.zy.lei@outlook.com>
2025-09-26 14:53:42 +0800

    init

---
+++ main/P1001.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+//#include<iostream>
+//#include<fstream>
+//#include<sstream>
+//#include<iomanip>
+
+//#include<vector>
+//#include<list>
+//#include<deque>
+//#include<map>
+//#include<set>
+//#include<unordered_map>
+//#include<unordered_set>
+//#include<stack>
+//#include<queue>
+//#include<array>
+
+//#include<algorithm>
+//#include<functional>
+//#include<iterator>
+//#include<cmath>
+//#include<math.h>
+//#include<numeric>
+//#include<string>
+//#include<cstring>
+//#include<memory>
+//#include<new>
+//#include<random>
+//#include<bitset>
+
+using namespace std;
+
+int main(){
+    int a,b;
+    scanf("%d%d",&a,&b);
+    printf("%d",a+b);
+    return 0;
+}
